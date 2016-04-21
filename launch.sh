d0c0d5c114eb9464b1bf2ec14b59490ed17f01b6
Romain Reuillon <romain.reuillon@iscpif.fr>
2016-04-21 20:50:11 +0200

    Make sure docker containers can use strace.

--- launch.sh
+++ launch.sh
@@ -1,11 +1,14 @@
 
+SCRIPT=`readlink -f $0`
+DIR=`dirname $SCRIPT`
+
 VOLUME=openmole_$1
 INSTANCE=openmole_$1
 
 shift
 
 docker volume create --name $VOLUME
-docker run --name $INSTANCE -h openmole --restart=always -p 8888 -id -v $VOLUME:/var/openmole -t openmole:latest $@ 
+docker run --security-opt "seccomp:$DIR/seccomp.json" --name $INSTANCE -h openmole --restart=always -p 8888 -id -v $VOLUME:/var/openmole -t openmole:latest $@ 
 
 PORT=`docker inspect --format='{{(index (index .NetworkSettings.Ports "8888/tcp") 0).HostPort}}' $INSTANCE`
 
