d00bc31cb5a29ba82f5d4bb4a4d842eb56dafff5
Romain Reuillon <romain.reuillon@iscpif.fr>
2016-05-05 13:35:57 +0200

    Add port argument.

--- launch.sh
+++ launch.sh
@@ -5,10 +5,13 @@ DIR=`dirname $SCRIPT`
 VOLUME=openmole_$1
 INSTANCE=openmole_$1
 
+PORT=$2
+
+shift
 shift
 
 docker volume create --name $VOLUME
-docker run --security-opt "seccomp:$DIR/seccomp.json" --name $INSTANCE -h openmole --restart=always -p 8888 -id -v $VOLUME:/var/openmole -t openmole:latest $@ 
+docker run --security-opt "seccomp:$DIR/seccomp.json" --name $INSTANCE -h openmole --restart=always -p $PORT:8888 -id -v $VOLUME:/var/openmole -t openmole:latest $@
 
 PORT=`docker inspect --format='{{(index (index .NetworkSettings.Ports "8888/tcp") 0).HostPort}}' $INSTANCE`
 
